9d081c84a277264db694b6c65041bdc277db6c94
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2024-10-01 20:42:37 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Jogo-da-adivinha-o-e-dados-em-android
---
+++ app/src/main/java/rj/senac/av1_dados/MainActivity.java
@@ -0,0 +1,17 @@
+package rj.senac.av1_dados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this, "Bem vindo!",Toast.LENGTH_SHORT).show();
+    }
+}
